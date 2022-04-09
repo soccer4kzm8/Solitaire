ee9455636d76b446be32174acb9b6eb4231ec6c2
soccer4kzm8 <soccer4.kzm8@gmail.com>
2022-04-09 15:03:33 +0900

    fix game clear description : adjust game clear object position and add text

---
+++ Assets/Scripts/AdjustPosition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustPosition : MonoBehaviour
+{
+    [SerializeField] private Transform _followedObj = null;
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(_followedObj.position.x,  gameObject.transform.position.y, _followedObj.position.z);
+    }
+}
